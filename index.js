fdd74882e20c718e13113e9ae5288c3762e90f68
raked <raked@users.noreply.github.com>
2021-06-10 10:32:55 -0400

    Boilerplate code to learn command handling

--- index.js
+++ index.js
@@ -23,21 +23,10 @@ client.on("message", (message) => {
 
   if (command === "ping") {
     message.channel.send("Pong.");
-  } else if (command === "prune") {
-    const amount = parseInt(args[0]) + 1;
-
-    if (isNaN(amount)) {
-      return message.reply("that doesn't seem to be a valid number.");
-    } else if (amount < 2 || amount > 100) {
-      return message.reply("you need to input a number between 2 and 100.");
-    }
-    message.channel.bulkDelete(amount, true).catch((err) => {
-      console.error(err);
-      message.channel.send(
-        "there was an error trying to prune messages in this channel!"
-      );
-    });
+  } else if (command === "beep") {
+    message.channel.send("Boop.");
   }
+  // ...
 });
 
 // login to Discord with your app's token
