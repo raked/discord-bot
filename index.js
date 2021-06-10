6969a203aa28846e90734fbb4570f93ae956e1af
raked <raked@users.noreply.github.com>
2021-06-10 09:52:01 -0400

    Added config file, made commands more flexible and complex

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const Discord = require("discord.js");
 const dotenv = require("dotenv");
 // create a new Discord client
 const client = new Discord.Client();
+// require config file
+const { prefix } = require("./config.json");
 
 dotenv.config();
 
@@ -14,9 +16,13 @@ client.once("ready", () => {
 
 //when a message is detected, log it to the console.
 client.on("message", (message) => {
-  if (message.content === "!ping") {
+  if (message.content.startsWith(`${prefix}ping`)) {
     // send back "Pong." to the channel the message was sent in
     message.channel.send("Pong.");
+  } else if (message.content.startsWith(`${prefix}beep`)) {
+    message.channel.send("Boop.");
+  } else if (message.content.startsWith(`${prefix}server`)) {
+    message.channel.send(`This server's name is: ${message.guild.name}`);
   }
 });
 
