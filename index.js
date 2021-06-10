622eade8193e11df500889e6d3b06346aa61dccd
raked <raked@users.noreply.github.com>
2021-06-10 10:40:49 -0400

    Added command handling

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+const fs = require("fs");
 // require discord.js
 const Discord = require("discord.js");
 const dotenv = require("dotenv");
 // create a new Discord client
 const client = new Discord.Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs
+  .readdirSync("./commands")
+  .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  // set a new item in the Collection
+  // with the key as the command name and the value as the exported module
+  client.commands.set(command.name, command);
+}
+
 // require config file
 const { prefix } = require("./config.json");
 
@@ -21,12 +35,14 @@ client.on("message", (message) => {
   const args = message.content.slice(prefix.length).trim().split(/ +/);
   const command = args.shift().toLowerCase();
 
-  if (command === "ping") {
-    message.channel.send("Pong.");
-  } else if (command === "beep") {
-    message.channel.send("Boop.");
+  if (!client.commands.has(command)) return;
+
+  try {
+    client.commands.get(command).execute(message, args);
+  } catch (error) {
+    console.error(error);
+    message.reply("there was an error trying to execute that command!");
   }
-  // ...
 });
 
 // login to Discord with your app's token
